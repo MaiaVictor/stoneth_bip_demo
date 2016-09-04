65241b2cde6895a5221cdf15adb14c2a540fde39
Victor Hernandes Silva Maia <srvictormaia@gmail.com>
2016-09-04 14:19:56 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var main = <div>Hello World</div>;
+
+window.onload = function(){
+  ReactDOM.render(main, document.getElementById("main"));
+};
